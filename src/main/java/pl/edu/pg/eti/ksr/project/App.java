ef5afeac5a28471d120ec3ec7022f6117b3a6ff4
Michal Flakowski <mflakowski.contact@gmail.com>
2022-04-05 20:22:21 +0200

    Empty project on Maven

---
+++ src/main/java/pl/edu/pg/eti/ksr/project/App.java
@@ -0,0 +1,13 @@
+package pl.edu.pg.eti.ksr.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
