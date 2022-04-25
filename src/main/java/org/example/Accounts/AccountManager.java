d25a0621afa1f4d639247e213cd508f49fc6720d
Majkelevsky <michbal222@gmail.com>
2022-04-25 18:39:18 +0200

    Log in Gui and rsa generation

---
+++ src/main/java/org/example/Accounts/AccountManager.java
@@ -0,0 +1,174 @@
+package org.example.Accounts;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.spec.KeySpec;
+import java.util.*;
+
+public class AccountManager {
+
+    private static Map<String, Integer> users = new HashMap<>();
+    private static String mainFilepath = "./BSK_files";
+
+    public static Map<String, Integer> getUsers() {
+        return users;
+    }
+
+    public static void populateAccounts() {
+        users.put("test", "test".hashCode());
+    }
+
+    public static void initialize() {
+        //Main folder
+        File f = new File(mainFilepath);
+        if (!f.exists()) {
+            f.mkdir();
+        }
+
+        //Accounts file
+        File fU = new File(mainFilepath + "/accounts.txt");
+        if (!fU.exists()) {
+            try {
+                fU.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //Private keys folder
+        File fPr = new File(mainFilepath + "/private");
+        if (!fPr.exists()) {
+            fPr.mkdir();
+        }
+
+        //Public keys folder
+        File fPu = new File(mainFilepath + "/public");
+        if (!fPu.exists()) {
+            fPu.mkdir();
+        }
+        readAccounts();
+    }
+
+    public static void readAccounts() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(mainFilepath + "/accounts.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                users.put(line, Integer.parseInt(reader.readLine()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addAccount(String username, Integer passHash) {
+        //Add user credentials
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(mainFilepath + "/accounts.txt", true));
+            writer.append(username);
+            writer.append('\n');
+            writer.append(passHash.toString());
+            writer.append('\n');
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        users.put(username, passHash);
+
+        //Generate keys
+        try {
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            generator.initialize(2048);
+            KeyPair pair = generator.generateKeyPair();
+
+            PrivateKey privateKey = pair.getPrivate();
+            PublicKey publicKey = pair.getPublic();
+
+            BufferedWriter writer = new BufferedWriter(
+                    new FileWriter(mainFilepath + "/private/" + username + "_private.txt", true));
+            String encryptedPriv = encrypt((Base64.getEncoder().encodeToString(privateKey.getEncoded())), passHash);
+            writer.append(encryptedPriv);
+            writer.append('\n');
+            writer.close();
+
+            BufferedWriter writer1 = new BufferedWriter(
+                    new FileWriter(mainFilepath + "/public/" + username + "_public.txt", true));
+            String encryptedPub = encrypt((Base64.getEncoder().encodeToString(publicKey.getEncoded())), passHash);
+            writer1.append(encryptedPub);
+            writer1.append('\n');
+            writer1.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String encrypt(String toEncrypt, Integer key) {
+        try {
+            //Generate key and iv
+            IvParameterSpec ivParams = generateIv();
+            SecretKey secretKey = getKeyFromPassword(key, 128, "12345678");
+
+            //Init cypher
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivParams);
+
+            //Encrypt
+            byte[] encrypted = cipher.doFinal(toEncrypt.getBytes());
+            return Base64.getEncoder().encodeToString(ivParams.getIV()) + " " + Base64.getEncoder().encodeToString(encrypted);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String decrypt(String toDecrypt, Integer key) {
+        String[] ivAndMess = getIvAndMessage(toDecrypt);
+        try {
+            //Get key and iv
+            IvParameterSpec ivParams = new IvParameterSpec(Base64.getDecoder().decode(ivAndMess[0]));
+            SecretKey secretKey = getKeyFromPassword(key, 128, "12345678");
+
+            //Init cypher
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParams);
+
+            //Decrypt
+            byte[] original = cipher.doFinal(Base64.getDecoder().decode(ivAndMess[1]));
+
+            return new String(original);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static SecretKey getKeyFromPassword(Integer passHash, int keySize, String salt) {
+        try {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(passHash.toString().toCharArray(), salt.getBytes(), 1000, keySize);
+            SecretKey secret = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+            return secret;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static IvParameterSpec generateIv() {
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
+
+    private static String[] getIvAndMessage(String k) {
+        return k.split(" ", 2);
+    }
+}
---
+++ src/main/java/org/example/PrimaryController.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import org.example.Accounts.AccountManager;
+
+public class PrimaryController {
+
+    @FXML
+    private Label communicate;
+
+    @FXML
+    private Button logIn;
+
+    @FXML
+    private TextField passwordInput;
+
+    @FXML
+    private Button signIn;
+
+    @FXML
+    private TextField usernameInput;
+
+    @FXML
+    void logInClicked(ActionEvent event) {
+        String username = usernameInput.getText();
+        if (AccountManager.getUsers().containsKey(username)) {
+            Integer passHash = passwordInput.getText().hashCode();
+            if (AccountManager.getUsers().get(username).equals(passHash)) {
+                communicate.setText("Logged as: " + username + "!");
+                communicate.setTextFill(Color.web("0x006400"));
+            } else {
+                communicate.setText("Password is wrong!");
+                communicate.setTextFill(Color.color(1,0,0));
+            }
+        } else {
+            communicate.setText("There is no such user!");
+            communicate.setTextFill(Color.color(1,0,0));
+        }
+    }
+
+    @FXML
+    void signInClicked(ActionEvent event) {
+        if (AccountManager.getUsers().containsKey(usernameInput.getText())) {
+            communicate.setText("Account with that username exists");
+            communicate.setTextFill(Color.color(1,0,0));
+        } else {
+            AccountManager.addAccount(usernameInput.getText(), passwordInput.getText().hashCode());
+            communicate.setText("Added account");
+            communicate.setTextFill(Color.web("0x006400"));
+        }
+    }
+
+}
